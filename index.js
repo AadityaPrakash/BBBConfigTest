d1b249a89d60ce4020b17ff71fc2d38c9e53b71d
Aaditya Prakash <aadi.appdeveloper@gmail.com>
2021-02-12 23:25:54 +0100

    Added lodash for merge Json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import YAML from 'yaml';
+import _ from 'lodash';
 
 const DEFAULT_SETTINGS = 'assets/default_settings.yaml';
 const LOCAL_SETTINGS = 'assets/local_settings.yaml';
@@ -9,18 +10,18 @@ const EMPTY_SETTINGS = 'assets/empty_settings.yaml';
 try {
   if (fs.existsSync(DEFAULT_SETTINGS)) {
     const SETTINGS = YAML.parse(fs.readFileSync(DEFAULT_SETTINGS, 'utf-8'));
-
+    console.log(SETTINGS);
     if (fs.existsSync(LOCAL_SETTINGS)) {
         console.log('Local configuration found! Overriding default configuration...');
         const L_SETTINGS = YAML.parse(fs.readFileSync(LOCAL_SETTINGS, 'utf-8'));
 
-        console.log(L_SETTINGS.public.app.appName);
-
+        _.merge(SETTINGS, L_SETTINGS);
 
     } else {
         console.log('Local Configuration not found! Loading default configuration...');
     }
 
+    console.log(SETTINGS);
 
   } else {
     throw new Error('File doesn\'t exists');
